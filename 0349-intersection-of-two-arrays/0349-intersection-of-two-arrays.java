3bde483249ac522bb2bb574b8b2729c6c9a004f7
tanishkkkaa <124276977+tanishkkkaa@users.noreply.github.com>
2024-05-16 22:06:53 +0530

    Time: 3 ms (58.78%), Space: 43 MB (77.96%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int p1 = 0;
+        int p2 = 0;
+        int index = 0;
+        int[] intersection = new int[Math.min(nums1.length,nums2.length)];
+        while(p1<nums1.length && p2<nums2.length){
+            if(nums1[p1]==nums2[p2]){
+                if(index==0 || intersection[index-1] != nums1[p1]){
+                    intersection[index++] = nums1[p1];
+                }
+                p1++;
+                p2++;
+            }else if(nums1[p1]<nums2[p2]){
+                p1++;
+            }else{
+                p2++;
+            }
+        }
+        return Arrays.copyOf(intersection, index);
+    }
+}
